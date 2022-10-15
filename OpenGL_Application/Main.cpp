c9753b5201252a0a60d78e1d79c025e481d4fa72
SafakOnol <onolsafak@outlook.com>
2022-10-15 16:42:00 -0400

    First commit

---
+++ OpenGL_Application/Main.cpp
@@ -0,0 +1,23 @@
+#include <glew.h>
+#include <glut.h>
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("Hello OpenGL!!\n");
+
+	glutInit(&argc, argv);
+	glutCreateWindow("GLEW Test");
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		/* Problem: glewInit failed, something is seriously wrong. */
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+
+	}
+	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+	getchar();
+
+	return 0;
+}
